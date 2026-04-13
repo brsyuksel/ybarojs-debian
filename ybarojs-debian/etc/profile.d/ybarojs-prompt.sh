b1e2e4ff2a5db55c1bdb7468eb2ad808c201dfab
Baris Yuksel <brsyuksel@gmail.com>
2026-04-13 21:13:59 +0200

    refactor: move PS1 from system-wide to user-specific .bashrc
    - Remove etc/profile.d/ybarojs-prompt.sh (system-wide approach)
    - Remove bash.bashrc modification logic from postinst/prerm
    - Add postinst logic to append PS1 to user's ~/.bashrc
      - Uses username collected during sudo setup
      - Creates .bashrc if doesn't exist
      - Checks for duplicates before adding
      - Sets proper ownership
    - Add prerm logic to remove PS1 from user .bashrc on uninstall
    - PS1 format: green(user@host):blue(dir) [yellow(exit code)]$

--- ybarojs-debian/etc/profile.d/ybarojs-prompt.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# System-wide bash prompt configuration for ybarojs-debian
-# This file is sourced by /etc/profile for all users
-# Sets a colored prompt with username, hostname, working directory, and exit code
-
-export PS1='\[\e[1;32m\]\u@\h\[\e[m\]:\[\e[1;34m\]\w\[\e[m\] [\[\e[1;33m\]$?\[\e[m\]]\$ '
